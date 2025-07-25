f0d9fcfa8217c06d7ff701a5ea2634c263fee19f
souleymanesy7 <souleymanesyservices@gmail.com>
2025-07-25 03:48:49 +0000

    feat: update the snacks plugins to show hidden and dotfile(.env) files

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,10 +1,33 @@
 -- Show ignored files (dotfiles)
 
+local excluded = {
+  "node_modules/",
+  "dist/",
+  ".vite/",
+  ".git/",
+
+  "package-lock.json",
+  "pnpm-lock.yaml",
+  "yarn.lock",
+}
+
 return {
-	"folke/snacks.nvim",
-	opts = {
-		picker = {
-			ignored = true,
-		},
-	},
+  "folke/snacks.nvim",
+  opts = {
+    notifier = { enabled = true },
+
+    -- show hidden files in snacks.explorer
+    picker = {
+      sources = {
+        explorer = {
+          -- show hidden files like .env
+          hidden = true,
+          -- show files ignored by git like node_modules
+          ignored = true,
+          -- exlude some file
+          exclude = excluded,
+        },
+      },
+    },
+  },
 }
